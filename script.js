068b17c1204cb6b19fa005b69e306cd740615d56
Jeanll7 <jeancarloleall@gmail.com>
2024-03-18 01:18:26 -0300

    scroll

--- script.js
+++ script.js
@@ -188,5 +188,22 @@ function updateRestaurantStatus() {
   }
 }
 
-updateRestaurantStatus()
+updateRestaurantStatus();
+
+const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" 
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) { 
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
 
