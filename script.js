0d8a4db4a449f90d3197d644bbb0325a41c564eb
Jeanll7 <jeancarloleall@gmail.com>
2024-03-20 02:04:03 -0300

    toast-center

--- script.js
+++ script.js
@@ -23,9 +23,8 @@ function showToast(message) {
     text: message,
     duration: 3000,
     close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
+    gravity: "top", 
+    position: "center", 
     style: {
       background: "#222",
       color: "#fff"
